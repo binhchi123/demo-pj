8ab8046e92382e6f0168504a061bf82774d83286
binhchi123 <chibinh890@gmail.com>
2023-04-27 16:38:17 +0700

    abc

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,24 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
+import { MainComponent } from './main/main.component';
+import { HomeComponent } from './home/home.component';
+import { ContactComponent } from './contact/contact.component';
+import { P404Component } from './p404/p404.component';
+import { AboutComponent } from './about/about.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    MainComponent,
+    HomeComponent,
+    ContactComponent,
+    P404Component,
+    AboutComponent
   ],
   imports: [
     BrowserModule,
